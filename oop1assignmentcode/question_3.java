dd5fd37ab37e6989e42e83c5f542a4cbc738bc6d
GavinWason <44084672+GavinWason@users.noreply.github.com>
2019-04-10 14:11:41 +0300

    Add files via upload

---
+++ oop1assignmentcode/question_3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_work_3;
+
+/**
+ *
+ * @author user
+ */
+public class question_3 {
+    public static void main (String [] args){
+        int [] array = new 
+        int [] {95,55,103,12,30,124,72,180,39,48,28,18,8,19,65,71,70,64,196,18,53};
+        
+        for(int i = 1; i<array.length; i++){
+            
+            if(array[i]%2!=0){
+            System.out.println(array[i]);
+            }
+        }
+    }
+    
+}
